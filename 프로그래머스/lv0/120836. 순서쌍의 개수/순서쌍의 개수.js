4fa2a6f45ed2f40da8c63f4c3f41d1b8138c797d
juninkorea95 <122579951+juninkorea95@users.noreply.github.com>
2023-01-20 22:15:50 +0900

    [level 0] Title: 순서쌍의 개수, Time: 4.79 ms, Memory: 36.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.js
@@ -0,0 +1,11 @@
+function solution(n) {
+    const result = []
+    for (let i=1; i <=n; i++) {
+        if (n % i ===0) result.push(i)
+    } return result.length
+}
+
+// 1부터 for문을 돌려 나머지가 0인 수를 찾는다. 
+// 제곱근보다 작은수 까지 돌린다. 
+// 제곱근까지 반복문 돌려서 나온 숫자 X2
+// 제곱근도 해당된다면 +1 아님 말고 
