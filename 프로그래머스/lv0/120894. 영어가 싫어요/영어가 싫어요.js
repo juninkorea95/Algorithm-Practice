986e29ba107a69ce8c67862ae86c5aca41e5e55a
juninkorea95 <122579951+juninkorea95@users.noreply.github.com>
2023-01-24 00:47:12 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.js
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.js
@@ -1,7 +1,22 @@
 function solution(numbers) {
-    const number = ["zero","one","two","three", "four", "five", "six", "seven", "eight", "nine"]
-    for(let i = 0 ; i<number.length; i++){
-        numbers = numbers.split(number[i]).join(i)
-    }
-    return +numbers
+    let A = ["zero", "one", "two", "three", "four", "five", "six","seven","eight","nine"]
+    for (let i=0; i<A.length; i++) {
+        numbers = numbers.split(A[i]).join(i)
+    } return +numbers
 }
+
+
+
+
+
+
+
+
+
+// function solution(numbers) {
+//     const number = ["zero","one","two","three", "four", "five", "six", "seven", "eight", "nine"]
+//     for(let i = 0 ; i<number.length; i++){
+//         numbers = numbers.split(number[i]).join(i)
+//     }
+//     return +numbers
+// }
