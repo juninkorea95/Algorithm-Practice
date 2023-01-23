0ec96f127429533adac73d9973e64d235e9fd3e4
juninkorea95 <122579951+juninkorea95@users.noreply.github.com>
2023-01-24 03:06:48 +0900

    [level 0] Title: 구슬을 나누는 경우의 수, Time: 0.12 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.js
+++ 프로그래머스/lv0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.js
@@ -1,25 +1,25 @@
-function solution(balls, share) {
-  let numerator = balls;
-  let denominator = share;
-
-  for (let i = 1; i < share; i++) {
-    numerator *= balls - i;
-    denominator *= share - i;
-  }
-  return numerator / denominator;
-}
-
 // function solution(balls, share) {
-//     let A = 1;
-//     for (let i=1; i<=balls; i++) {
-//         A = A * i
-//     }
-//     let B = 1;
-//     for (let i=1; i<=balls-share; i++) {
-//         B = B * i
-//     }
-//     let C = 1;
-//     for (let i=1; i<=share; i++) {
-//         C = C * i
-//     } return A / (B * C)
+//   let numerator = balls;
+//   let denominator = share;
+
+//   for (let i = 1; i < share; i++) {
+//     numerator *= balls - i;
+//     denominator *= share - i;
+//   }
+//   return numerator / denominator;
 // }
+
+function solution(balls, share) {
+    let A = 1;
+    for (let i=1; i<=balls; i++) {
+        A = A * i
+    }
+    let B = 1;
+    for (let i=1; i<=balls-share; i++) {
+        B = B * i
+    }
+    let C = 1;
+    for (let i=1; i<=share; i++) {
+        C = C * i
+    } return Math.round(A / (B * C))
+}
