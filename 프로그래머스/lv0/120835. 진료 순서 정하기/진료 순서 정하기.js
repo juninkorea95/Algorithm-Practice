bcc8fc47d2928637bed22f871f795dd446ee009b
juninkorea95 <122579951+juninkorea95@users.noreply.github.com>
2023-01-23 20:58:10 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.js
+++ 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.js
@@ -1,8 +1,15 @@
 function solution(emergency) {
-    let order = new Map();
-    let desc = [...emergency].sort((a,b) => b-a);
-    for(let i = 0; i < desc.length; i++){
-        order.set(desc[i], i+1);   
-    }
-    return emergency.map(v => order.get(v));
+    let newArr = emergency.slice('').sort((a,b) => b-a)
+    return emergency.map(x => newArr.indexOf(x)+1)
 }
+
+
+// function solution(emergency) {
+//     let sorted = emergency.slice().sort((a,b)=>b-a);
+//     return emergency.map(v=>sorted.indexOf(v)+1);
+// }
+
+// 중간에 .slice('')를 사용하는 이유
+// -> 빈 공간을 슬라이스했으니 원형은 유지하면서, emergency의 원형을 깨지 않고 새로운 배열을 만든다는 
+// 메리트가 있다. 
+// 마지막 부분에 map 씌울 때는 본 함수 emergency에 씌워야 한다.
