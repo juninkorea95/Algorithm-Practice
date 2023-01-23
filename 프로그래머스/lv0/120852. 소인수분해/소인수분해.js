8c45c83a413f463ccda2fabcad1cb9baf26bf3c6
juninkorea95 <122579951+juninkorea95@users.noreply.github.com>
2023-01-24 00:28:52 +0900

    [level 0] Title: 소인수분해, Time: 0.04 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120852. 소인수분해/소인수분해.js
+++ 프로그래머스/lv0/120852. 소인수분해/소인수분해.js
@@ -1,15 +1,30 @@
 function solution(n) {
-    var answer = [];
-
-    for(let i = 2; i <= n; i++) {
-
-        while (n % i === 0) {
-
-            n = n / i;
-            answer.push(i);
-
+    let answer = []
+    for (let i=2; i <=n; i++) {
+        while (n % i ===0) {
+            n = n / i
+            answer.push(i)
         }
-    }
-
-    return [...new Set(answer)];
+    } return [...new Set(answer)]
 }
+
+
+
+
+
+
+// function solution(n) {
+//     var answer = [];
+
+//     for(let i = 2; i <= n; i++) {
+
+//         while (n % i === 0) {
+
+//             n = n / i;
+//             answer.push(i);
+
+//         }
+//     }
+
+//     return [...new Set(answer)];
+// }
