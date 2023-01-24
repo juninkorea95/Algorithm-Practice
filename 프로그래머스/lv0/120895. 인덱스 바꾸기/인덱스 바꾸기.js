78684bea63e10453663d00935c06e16b38c8126d
juninkorea95 <122579951+juninkorea95@users.noreply.github.com>
2023-01-25 01:46:07 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
@@ -0,0 +1,10 @@
+function solution(my_string, num1, num2) {
+    let Arr = my_string.split('')
+    let a = Arr[num1];
+    Arr[num1] = Arr[num2];  // value num1 converts to num2 
+    Arr[num2] = a           // num2 takes the value from let a = Arr[num1]
+    return Arr.join('')
+    
+}
+
+// 두 원소 순서바꾸기 식 두줄 왜 저렇게 전개되는지 좀 더 알아봐야함.
