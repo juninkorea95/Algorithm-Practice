8e2ac40e8ed919c13a1c19d45c10b7cda3f77243
juninkorea95 <122579951+juninkorea95@users.noreply.github.com>
2023-01-25 01:47:01 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
+++ 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
@@ -8,3 +8,9 @@ function solution(my_string, num1, num2) {
 }
 
 // 두 원소 순서바꾸기 식 두줄 왜 저렇게 전개되는지 좀 더 알아봐야함.
+
+// function solution(my_string, num1, num2) {
+//     my_string = my_string.split('');
+//     [my_string[num1], my_string[num2]] = [my_string[num2], my_string[num1]];
+//     return my_string.join('');
+// }
