8a6fde738dd45e6cee07c1a2422b1565610e8acc
juninkorea95 <122579951+juninkorea95@users.noreply.github.com>
2023-01-24 13:44:46 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120853. 컨트롤 제트/컨트롤 제트.js
+++ 프로그래머스/lv0/120853. 컨트롤 제트/컨트롤 제트.js
@@ -1,17 +1,20 @@
 function solution(s) {
-    s = s.split(" ")
-    // Z가 있다면, 이전것과 같이 삭제
-    while(s.includes('Z')) {
-        s.splice(s.indexOf('Z')-1,2)
+    let A = s.split(' ')
+    
+    while (A.includes('Z')) {
+        A.splice(A.indexOf('Z')-1,2)
     }
-    return s.reduce((a,b) => Number(a)+Number(b), 0)
+    return A.reduce((a,b) => Number(a) + Number(b),0)
 }
 
+
+
+
 // function solution(s) {
-//     let A = s.split(' ')
-//     for (let i=0; i < A.length; i++) {
-//         if (A[i] === 'Z') {
-//             A.splice(i-1,2)
-//         }
-//     } return A
+//     s = s.split(" ")
+//     // Z가 있다면, 이전것과 같이 삭제
+//     while(s.includes('Z')) {
+//         s.splice(s.indexOf('Z')-1,2)
+//     }
+//     return s.reduce((a,b) => Number(a)+Number(b), 0)
 // }
