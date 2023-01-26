c31c242311df16061e405c2dcaf1a67e9aeffbf8
juninkorea95 <122579951+juninkorea95@users.noreply.github.com>
2023-01-27 02:19:53 +0900

    [level 1] Title: 약수의 합, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12928. 약수의 합/약수의 합.js
+++ 프로그래머스/lv1/12928. 약수의 합/약수의 합.js
@@ -1,13 +1,24 @@
-function solution(n) {
+// function solution(n) {
+//     let result = 0;
+//     let Answer = []
+//     for (let i=1; i<=n; i++) {
+//         if (n % i === 0) {
+//             Answer.push(i)
+//         }
+//     }
+//     for (element of Answer) {
+//         result += element
+//     }
+//     return result 
+// }
+
+// 위 풀이는 바보같은 짓이다...배열만들고 거기서 요소 또 뽑아서 더할 필요가 없다..
+
+const solution = function(n) {
     let result = 0;
-    let Answer = []
     for (let i=1; i<=n; i++) {
-        if (n % i === 0) {
-            Answer.push(i)
+        if (n % i ===0) {
+            result += i
         }
-    }
-    for (element of Answer) {
-        result += element
-    }
-    return result 
+    } return result 
 }
