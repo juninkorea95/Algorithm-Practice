aae464ef5d42caec06217b1fde064f1c6207091d
juninkorea95 <122579951+juninkorea95@users.noreply.github.com>
2023-01-27 00:19:20 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120884. 치킨 쿠폰/치킨 쿠폰.js
+++ 프로그래머스/lv0/120884. 치킨 쿠폰/치킨 쿠폰.js
@@ -6,3 +6,6 @@ function solution(chicken) {
         chicken = Math.floor((chicken / 10) + (chicken % 10))
     } return answer
 }
+
+// 치킨 나누기 10과 치킨을 10으로 나누었을 때의 나머지를 더해서 그 다음 쿠폰수를 환산하는 아이디어 
+// 변수를 쿠폰이라고 했으면 더 알아보기 쉬웠을듯 
