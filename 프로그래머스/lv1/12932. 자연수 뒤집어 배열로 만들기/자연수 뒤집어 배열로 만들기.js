b88b81719fc0ba5400165116141a9de0e5d66ef9
juninkorea95 <122579951+juninkorea95@users.noreply.github.com>
2023-01-27 10:53:34 +0900

    [level 1] Title: 자연수 뒤집어 배열로 만들기, Time: 0.08 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.js
+++ 프로그래머스/lv1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.js
@@ -6,3 +6,10 @@ function solution(n) {
         answer.push(A[i])
     } return answer.map(x=>parseInt(x))
 }
+
+// 숫자를 문자열화 시켜서 스플릿
+// 문자화된 요소를 다시 parseInt로 숫자로 돌려놓음
+// 역for문 적용하여 Arr 끝 요소를 answer의 첫 요소로 들어가게끔 설정
+// 근데 돌려보니 answer의 요소가 문자처럼 나옴..(왜?!?!)
+// 그래서 map으로 각 요소를 숫자취급을 다시 해줌 parseInt로
+// 답이 나오긴 했는데 마지막 부분에 왜 요소가 문자로 나왔을까..몰름ㅋㅋ
