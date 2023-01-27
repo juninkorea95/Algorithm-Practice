04d78fbfa78295efbe11564aaa3f8252fcabf22a
juninkorea95 <122579951+juninkorea95@users.noreply.github.com>
2023-01-27 15:36:08 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.21 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
+++ 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
@@ -1,10 +1,15 @@
-const solution = function(arr) {
+function solution(arr) {
+    let answer = []
     let A = Math.min(...arr)
-    arr.splice(arr.indexOf(A),1)
-    arr.length <= 1 ? answer =[-1] : answer = arr
     
-    return answer
-}
+    for (i=0; i<=arr.length; i++) {
+        if (arr[i] === A) {
+            delete arr[i]
+        }
+    } 
+     answer =  arr.filter(v=>v)
+    return answer.length === 0 ? [-1] : answer;
+}    
 
 // splice 하여 최솟값의 인덱스를 찾아내어 한개만 잘라낸다. 
 // 마지막 삼항연산자 설명이 이해가 안됨..
