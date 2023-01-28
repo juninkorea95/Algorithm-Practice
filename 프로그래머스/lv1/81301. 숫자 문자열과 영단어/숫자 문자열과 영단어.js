12aaf0fd56d83da61584e89b524f69f3c3366c6d
juninkorea95 <122579951+juninkorea95@users.noreply.github.com>
2023-01-28 20:16:44 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -1,5 +1,5 @@
 function solution(s) {
-    if (parseInt(s) === Number(s)) return Number(s) // s와 result가 같을때 
+    // if (parseInt(s) === Number(s)) return Number(s) // s와 result가 같을때 
     let numbers = ['zero','one','two','three','four','five','six','seven','eight','nine']
     for (i=0; i < numbers.length; i++) {
         s = s.split(numbers[i]).join(i)
