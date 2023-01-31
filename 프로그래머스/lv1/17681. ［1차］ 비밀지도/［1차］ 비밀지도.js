6e588bef5fb2e1bae684c5e93df7a9d13c8b6687
juninkorea95 <122579951+juninkorea95@users.noreply.github.com>
2023-01-31 11:36:02 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -0,0 +1,20 @@
+function solution(n, arr1, arr2) {
+  const answer = [];
+  const A = arr1.map((num) => num.toString(2).padStart(n, 0));
+  const B = arr2.map((num) => num.toString(2).padStart(n, 0));
+  const sumArr = [];
+  for (let i = 0; i < n; i++) {
+    sumArr.push(String(+A[i] + +B[i]).padStart(n, 0));
+  }
+  for (let num of sumArr) {
+    answer.push(
+      num
+        .split("")
+        .map((item) => (item === "0" ? (item = " ") : (item = "#")))
+        .join("")
+    );
+  }
+
+  return answer;
+}
+
