65d8a002861199806c7a5714aded289ad085c9c6
juninkorea95 <122579951+juninkorea95@users.noreply.github.com>
2023-05-31 21:21:53 +0900

    [level 1] Title: 내적, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/70128. 내적/내적.js
+++ 프로그래머스/lv1/70128. 내적/내적.js
@@ -1,7 +1,12 @@
+// function solution(a, b) {
+//     let sum = 0;
+//     for (let i = 0; i <= a.length -1; i ++) {
+//         sum += a[i] * b[i]
+//     }
+//     return sum 
+// }
+
 function solution(a, b) {
-    let sum = 0;
-    for (let i = 0; i <= a.length -1; i ++) {
-        sum += a[i] * b[i]
-    }
-    return sum 
+    var answer = a.reduce((acc, cur, idx) => acc += cur*b[idx], 0)
+    return answer;
 }
