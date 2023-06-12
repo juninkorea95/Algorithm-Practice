51504402bdd1380eaebb01775fd95657f573f49f
juninkorea95 <122579951+juninkorea95@users.noreply.github.com>
2023-06-12 23:25:46 +0900

    [unrated] Title: 과일 장수, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/135808. 과일 장수/과일 장수.js
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.js
@@ -1,13 +1,22 @@
+// function solution(k, m, score) {
+//     var answer = 0;
+//     let scoreIdx = 0
+//     score.sort((a,b)=>b-a)
+//     while(scoreIdx < score.length){
+//         const newSlice = score.slice(scoreIdx,scoreIdx+m)
+//         scoreIdx+= m
+//         if(newSlice.length === m){
+//             answer += (Math.min(...newSlice) * m)
+//         }
+//     }
+//     return answer;
+// }
+
 function solution(k, m, score) {
-    var answer = 0;
-    let scoreIdx = 0
-    score.sort((a,b)=>b-a)
-    while(scoreIdx < score.length){
-        const newSlice = score.slice(scoreIdx,scoreIdx+m)
-        scoreIdx+= m
-        if(newSlice.length === m){
-            answer += (Math.min(...newSlice) * m)
-        }
+    let answer = 0;
+    const sortedScore = score.slice().sort((a, b) => a - b).slice(score.length % m);
+    for (let i = 0; i < sortedScore.length; i += m) {
+        answer += sortedScore[i] * m;
     }
     return answer;
 }
