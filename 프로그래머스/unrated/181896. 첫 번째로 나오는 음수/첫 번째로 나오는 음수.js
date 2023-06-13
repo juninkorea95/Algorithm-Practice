d87d824aa6b5f951d4e93e0633f3c62badeaf052
juninkorea95 <122579951+juninkorea95@users.noreply.github.com>
2023-06-13 20:36:16 +0900

    [unrated] Title: 첫 번째로 나오는 음수, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.js
+++ 프로그래머스/unrated/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.js
@@ -1,9 +1,23 @@
+// function solution(num_list) {
+//     let answer = [];
+//     for (let i=0; i < num_list.length; i++) {
+//         if (num_list[i] < 0) {
+//             answer.push(i)
+//         } 
+//     }
+//     return answer.length === 0 ? -1 : answer[0]
+// }
+
 function solution(num_list) {
-    let answer = [];
-    for (let i=0; i < num_list.length; i++) {
-        if (num_list[i] < 0) {
-            answer.push(i)
-        } 
+    let answer = 0;
+    for (let i =0; i < num_list.length; i++) {
+        if (num_list[i] <0) {
+            answer = i
+            break;
+        }
+        else {
+            answer = -1 
+        }
     }
-    return answer.length === 0 ? -1 : answer[0]
+    return answer
 }
