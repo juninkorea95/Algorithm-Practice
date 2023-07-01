d24061f82de19c4643d95fbfea5068ce66f95c9f
juninkorea95 <122579951+juninkorea95@users.noreply.github.com>
2023-07-01 22:52:48 +0900

    [level 0] Title: 옹알이 (1), Time: 0.23 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.js
+++ 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.js
@@ -1,39 +1,33 @@
 function solution(babbling) {
-  const patterns = ["aya", "ye", "woo", "ma"];
-  let arr = [];
-
-  babbling.map(element => {
-    let modifiedElement = element;
-    let foundPattern = true;
-
-    while (foundPattern) {
-      foundPattern = false;
-
-      for (const pattern of patterns) {
-        if (modifiedElement.startsWith(pattern)) {
-          modifiedElement = modifiedElement.replace(pattern, "");
-          foundPattern = true;
-          break;
+   let arr = []; 
+   const patterns = ["aya", "ye", "woo", "ma"];
+  
+    babbling.map(word => {
+        let newWord = word;
+        let foundPattern = true;
+        
+        while (foundPattern) {
+            foundPattern = false;
+            
+            for (const pattern of patterns) {
+                if (newWord.startsWith(pattern)) {
+                    newWord = newWord.replace(pattern, "");
+                    foundPattern = true;
+                    break;
+                }
+            }
         }
-      }
-    }
-
-    arr.push(modifiedElement);
-  });
-
- let result = 0;
-arr.forEach(x => {
-  if (x === "") {
-    result += 1;
-  }
-});
-return result;
+        
+        arr.push(newWord)
+        
+    })
     
+    let result = 0; 
+    arr.forEach (x => {
+        if (x ==="") {
+            result += 1
+        }
+    })
     
+    return result 
 }
-
-
-
-
-
-
